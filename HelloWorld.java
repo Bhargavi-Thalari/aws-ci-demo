7d1a207a46500873aeb99a3e1dcb765fe621ccbc
Bhargavi-Thalari <thalaribhargavi09@gmail.com>
2026-06-04 13:48:49 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from AWS CI/CD Pipeline!");
+        System.out.println("Build successful.");
+    }
+}
